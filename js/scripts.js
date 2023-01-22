cb12c00fe02ff3420fc7a1e3b330a5095dbb0e29
Frode Aabak <frode_aabak@hotmail.com>
2023-01-22 16:42:01 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,12 @@ xmlHttp.open( "GET", "subpages/" + thePage + ".html", false );
 xmlHttp.send( null );
 document.write(xmlHttp.responseText);
 
+
+const pages = ["home", "portfolio", "contact"];
+const titles = ["Home", "Portfolio", "Contact"];
+
+
+
 var navigationCode = "";
 
 document.getElementById("navigationUL").innerHTML= navigationCode;
