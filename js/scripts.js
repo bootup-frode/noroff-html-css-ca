66c4f7224d16311baa2b0f2adae4ac24ffd0d139
Frode Aabak <frode_aabak@hotmail.com>
2023-01-22 16:27:46 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
 function getPage(thePage)
-  {
-    var xmlHttp = null;
-
-    xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", "subpages/" + thePage + ".html", false );
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-  }
+{
+var xmlHttp = null;
+xmlHttp = new XMLHttpRequest();
+xmlHttp.open( "GET", "subpages/" + thePage + ".html", false );
+xmlHttp.send( null );
+return xmlHttp.responseText;
+}
