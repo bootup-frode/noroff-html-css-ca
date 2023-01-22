cd64282bd922f6847a9185582dbcc114a0e1b40b
Frode Aabak <frode_aabak@hotmail.com>
2023-01-22 16:57:36 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,20 @@ xmlHttp.open( "GET", "subpages/" + thePage + ".html", false );
 xmlHttp.send( null );
 document.getElementById("pageContent").innerHTML = xmlHttp.responseText;
 
-const pageFiles = ["home", "portfolio", "contact"];
-const pageNames = ["Home", "Portfolio", "Contact"];
 
-
+// ----------------------------
+// A static menu would look like this:
+// ----------------------------
 // <li><span class="navItemCurrent">Home</span></li>
 // <li><a href="#" onclick="pageLoader('portfolio');">Portfolio</a></li>
 // <li><a href="#" onclick="pageLoader('contact');">Contact</a></li>
+// ----------------------------
+// But here we will create it dynamically. :)
+// ----------------------------
 
+const pageFiles = ["home", "portfolio", "contact"];
+const pageNames = ["Home", "Portfolio", "Contact"];
 var navigationCode = "";
-
 let i = 0;
 
 while (i < pageFiles.length)
