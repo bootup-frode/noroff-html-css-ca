0ac4d8a4183df22532c429cd202dc92f9100c5ca
Frode Aabak <frode_aabak@hotmail.com>
2023-01-22 16:44:27 +0100

    Update pageloader

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,9 @@ const pages = ["home", "portfolio", "contact"];
 const titles = ["Home", "Portfolio", "Contact"];
 
 
+// <li><span class="navItemCurrent">Home</span></li>
+// <li><a href="#" onclick="loadPage('portfolio');">Portfolio</a></li>
+// <li><a href="#" onclick="loadPage('contact');">Contact</a></li>
 
 var navigationCode = "";
 
